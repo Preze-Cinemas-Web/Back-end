49673d4c517d92e0dfa9c59ea71605ad440b1b9a
Tiger <101432787+Ath21@users.noreply.github.com>
2024-01-02 17:08:38 +0200

    Ath21/Removing Http Get etc.

--- CinemaStore/Controllers/UsersController.cs
+++ CinemaStore/Controllers/UsersController.cs
@@ -1,15 +1,14 @@
-﻿/*
- *  Οι controller χρησιμοποιούνται για να επικοινωνούν με τα APIs, εδώ θα εκτελέσουμε τις μεθόδους του 
- *  HTTP (GET, POST, PUT, PATCH, DELETE), ώστε να υλοποιήσουμε την μέθοδο CRUD (Create Read Update Delete).
- *  Στα ελληνικά... εδώ θα αλληλεπιδρούμε με την βάση μας:
- *  1) Θα δημιουργούμε μία εγγραφή (Create)
- *  2) Θα διαβάζουμε μία εγγραφή   (Read)
- *  3) Θα ενημερώνουμε μία εγγραφή (Update)
- *  4) θα διαγράφουμε μία εγγραφή  (Delete)
- */
+/*
+*  Οι controller χρησιμοποιούνται για να επικοινωνούν με τα APIs, εδώ θα εκτελέσουμε τις μεθόδους του 
+*  HTTP (GET, POST, PUT, PATCH, DELETE), ώστε να υλοποιήσουμε την μέθοδο CRUD (Create Read Update Delete).
+*  Στα ελληνικά... εδώ θα αλληλεπιδρούμε με την βάση μας:
+*  1) Θα δημιουργούμε μία εγγραφή (Create)
+*  2) Θα διαβάζουμε μία εγγραφή   (Read)
+*  3) Θα ενημερώνουμε μία εγγραφή (Update)
+*  4) θα διαγράφουμε μία εγγραφή  (Delete)
+*/
 
 using Cinema.Models;
-using CinemaData;
 using CinemaStore.Business;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,49 +16,15 @@ namespace Cinema.Controllers
 {
     [ApiController]
     [Route("/Ath21's API/1.0/[controller]")]
-    public class UsersController : ControllerBase
+    public class UserController : ControllerBase
     {
         private readonly ILogger _logger;
-        private readonly IUsersService _usersService;
+        private readonly IUserService _usersService;
 
-        public UsersController(ILogger<UsersController> logger, IUsersService usersService)
+        public UserController(ILogger<UserController> logger, IUserService usersService)
         {
             _logger = logger;
             _usersService = usersService;
-        }
-
-        [HttpGet]
-        public IEnumerable<UsersDTO> GetUsers()
-        {
-            return _usersService.GetAllUsers();
-        }
-
-        [HttpGet("{id:int}")]
-        public ActionResult<UsersDTO> GetOneUser(int id) 
-        {
-            var user = _usersService.GetUserById(id);
-
-            if (user != null)
-            {
-                return user; 
-            }
-            else
-            {
-                return NotFound();
-            }
-        }
-
-        [HttpPost]
-        public ActionResult<UsersDTO> CreateOneUser(UsersDTO user) 
-        {
-            try
-            {
-                return _usersService.CreateUser(user);
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
         }
     }
 }
