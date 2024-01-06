96549a35b65ff8c930f5e90ae58f846a3930c4d3
Tiger <billath131908@gmail.com>
2024-01-06 20:26:51 +0200

    Enable CORS with default policy and middleware

--- CinemaStore/Startup.cs
+++ CinemaStore/Startup.cs
@@ -22,6 +22,7 @@ namespace CinemaStore
          *  [1] Το ORM (CinemaData/CinemaContext.cs)
          *  [2] Τις υπηρεσίες για το Business Logic(CinemaStore/Business/IUserService.cs, UserService.cs)
          *  [3] Τον mapper για την μετατροπή από data σε store και αντίστροφα (CinemaStore/CinemaStoreProfile.cs)
+         *  [4] CORS για την επικοινωνία client με server https://learn.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-6.0
          */
         public void ConfigureServices(IServiceCollection services)
         {
@@ -33,6 +34,19 @@ namespace CinemaStore
                 typeof(IUserService), typeof(UserService));
             /****** [3] ******/
             services.AddAutoMapper(typeof(CinemaStoreProfile));
+            /****** [4] ******/
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    policy =>
+                    {
+                        policy.WithOrigins("https://localhost:7236", 
+                                           "http://localhost:5139",
+                                           "Client's URL...");
+                    });
+            });
+
+
             services.AddControllers();
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
@@ -45,6 +59,8 @@ namespace CinemaStore
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseCors();
 
             app.UseHttpsRedirection();
 
