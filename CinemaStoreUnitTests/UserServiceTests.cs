d142450b613751cb02a6f72657a6be375b3e6b55
Tiger <billath131908@gmail.com>
2024-01-09 05:32:41 +0200

    UserServiceNameTest Completed

--- CinemaStoreUnitTests/UserServiceTests.cs
+++ CinemaStoreUnitTests/UserServiceTests.cs
@@ -1,4 +1,4 @@
-using AutoMapper;
+﻿using AutoMapper;
 using Cinema.Models;
 using CinemaData;
 using CinemaStore.Business;
@@ -35,6 +35,70 @@ namespace CinemaStoreUnitTests
             };
 
             Assert.Throws<MyException>(() => _userService.CreateUser(userDTO));
+        }
+
+        [Test]
+        public void UserServiceNameTest()
+        {
+            // [3] Πρέπει να περιέχει μόνο γράμματα
+            UserDTO userDTO1 = new UserDTO()
+            {
+                Id = 0,
+                FirstName = "George123",
+            };
+            UserDTO userDTO2 = new UserDTO()
+            {
+                Id = 0,
+                FirstName = "George",
+                LastName = "Prezerakos123"
+            };
+            Assert.Throws<MyException>(() => _userService.CreateUser(userDTO1));
+            Assert.Throws<MyException>(() => _userService.CreateUser(userDTO2));
+
+            // [4] Το 1ο γράμμα πρέπει να είναι κεφαλαίο
+            UserDTO userDTO3 = new UserDTO()
+            {
+                Id = 0,
+                FirstName = "george"
+            };
+            UserDTO userDTO4 = new UserDTO()
+            {
+                Id = 0,
+                FirstName = "George",
+                LastName = "prezerakos"
+            };
+            Assert.Throws<MyException>(() => _userService.CreateUser(userDTO3));
+            Assert.Throws<MyException>(() => _userService.CreateUser(userDTO4));
+
+            // [5] Τα γράμματα εκτός από το 1ο, πρέπει να είναι πεζά
+            UserDTO userDTO5 = new UserDTO()
+            {
+                Id = 0,
+                FirstName = "GeOrge",
+            };
+            UserDTO userDTO6 = new UserDTO()
+            {
+                Id = 0,
+                FirstName = "George",
+                LastName = "PrEzErakos"
+            };
+            Assert.Throws<MyException>(() => _userService.CreateUser(userDTO5));
+            Assert.Throws<MyException>(() => _userService.CreateUser(userDTO6));
+
+            // [6] Τα γράμματα πρέπει να είναι όλα λατινικά
+            UserDTO userDTO7 = new UserDTO()
+            {
+                Id = 0,
+                FirstName = "Γιώργος",
+            };
+            UserDTO userDTO8 = new UserDTO()
+            {
+                Id = 0,
+                FirstName = "George",
+                LastName = "Πρεζεράκος"
+            };
+            Assert.Throws<MyException>(() => _userService.CreateUser(userDTO5));
+            Assert.Throws<MyException>(() => _userService.CreateUser(userDTO6));
         }
     }
 }
