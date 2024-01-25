07629f5e9d9792ea6ff7729a938b26952c021a3e
Tiger <billath131908@gmail.com>
2024-01-25 09:53:53 +0200

    Create model OldUser for Login

---
+++ CinemaData/OldUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaData
+{
+    public class OldUser
+    {
+        public string existingUsername { get; set; }
+        public string existingPassword { get; set; }
+    }
+}
---
+++ CinemaStore/Models/OldUserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace CinemaStore.Models
+{
+    public class OldUserDTO
+    {
+        public string existingUsername { get; set; }
+        public string existingPassword { get; set; }
+    }
+}
