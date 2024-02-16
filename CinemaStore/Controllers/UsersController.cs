11bc489ef6eb4f2602af4b06a4b5c30372a06190
Tiger <billath131908@gmail.com>
2024-02-16 20:03:11 +0200

    UserController

---
+++ CinemaStore/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Cinema.Models;
+using CinemaStore.Business;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaStore.Controllers
+{
+    [Authorize]
+    [Route("API/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Route("Get-All-Users")]
+        public ActionResult<IEnumerable<RegisterUserDTO>> GetAllUsers()
+        {
+            var users = _userService.FindAllUsers();
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("Get-User/{username}")]
+        public ActionResult<RegisterUserDTO> GetUser(string username)
+        {
+            var user = _userService.FindUserByUsername(username);
+            if (user == null)
+                return NotFound();
+            return Ok(user);
+        }
+    }
+}
