ffbc3987335cd424e87d1199fe301954138e7c38
Tiger <billath131908@gmail.com>
2024-04-02 00:59:08 +0300

    get all reservations

---
+++ CinemaStoreIntegrationTests/ControllerTests/ReservationsControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using CinemaStore;
+using CinemaStore.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+
+namespace CinemaStoreIntegrationTests.ControllerTests
+{
+    public class ReservationsControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private WebApplicationFactory<Program> _factory;
+        private const string EnvironmentVariable = "ASPNETCORE_ENVIRONMENT";
+
+        public ReservationsControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            Environment.SetEnvironmentVariable(EnvironmentVariable, "Staging");
+        }
+
+        [Fact]
+        public async Task GetAllReservations()
+        {
+            var route = "https://localhost:7236/API/Reservations/Get-All-Reservations";
+            var client = _factory.CreateClient();
+
+            var result = await TestUtilities.Get(client, route);
+            var reservations = await ReadReservations(result);
+
+            Assert.True(reservations.Count() >= 0);
+        }
+
+        private async Task<IEnumerable<ReservationDTO>> ReadReservations(HttpResponseMessage result)
+        {
+            Assert.True(result.IsSuccessStatusCode);
+
+            var content = await result.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<IEnumerable<ReservationDTO>>(content);
+        }
+    }
+}
